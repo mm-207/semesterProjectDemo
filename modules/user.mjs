9a8f3b9861b7fcfa5c22548c95d9170b7c2faba7
Christian Simonsen <crsdev@gmail.com>
2024-01-15 11:56:33 +0100

    #5 Staterd working on server

---
+++ modules/user.mjs
@@ -0,0 +1,10 @@
+class User {
+
+    constructor() {
+        this.email;
+        this.pswHash;
+        this.nick;
+    }
+}
+
+export default User;
--- server.mjs
+++ server.mjs
@@ -1,15 +1,22 @@
 import express from 'express'
+import User from './modules/user.mjs';
 
 const server = express();
+
+
+
 const port = (process.env.PORT || 8080);
 
 server.set('port', port);
 server.use(express.static('public'));
-server.get("/", (req, res, next) => {
 
-    res.status(200).send('Hello World').end();
+server.get("/", getRoot);
 
-});
+function getRoot(req, res, next) {
+    res.status(200).send(JSON.stringify({ msg: "These are nto the droids...." })).end();
+}
+
+
 
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
